dfa4e185d9f5ea5f2ad67b491c61a643be6868b0
jeanne <jbertoux@D203-4.Tertiaire.loges>
2016-09-28 11:40:39 +0200

    test

---
+++ src/Capital1.java
@@ -0,0 +1,23 @@
+
+public class Capital1 extends Ville{
+	
+
+	
+	public Capital(String unNom, int unNbHabitant, int uneSuperficie){
+		super(unNom, unNbHabitant,uneSuperficie);
+		
+		
+	}
+	
+
+	
+	public String toString(){
+		String chaine;
+		chaine = "Le nom est: " + super.getNom();
+		chaine += "\nLe nombre d'habitants: " + super.getNbHabitants() + "\nsuperficie: " + super.getSuperficie();
+		return chaine;
+		
+	}
+
+}
+// bla bla
