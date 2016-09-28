6c48700901f8dab6f592d65dd893c2e894ccc6c0
jeanne <jbertoux@D203-4.Tertiaire.loges>
2016-09-28 11:33:04 +0200

    premier commit du fichier capital du tp java

---
+++ src/Capital.java
@@ -0,0 +1,21 @@
+public class Capital extends Ville{
+	
+
+	
+	public Capital(String unNom, int unNbHabitant, int uneSuperficie){
+		super(unNom, unNbHabitant,uneSuperficie);
+		
+		
+	}
+	
+
+	
+	public String toString(){
+		String chaine;
+		chaine = "Le nom est: " + super.getNom();
+		chaine += "\nLe nombre d'habitants: " + super.getNbHabitants() + "\nsuperficie: " + super.getSuperficie();
+		return chaine;
+		
+	}
+
+}
